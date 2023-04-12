99e5459a6e5bb1aed5a7c48a91b3671f8d012d53
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-12 20:31:31 +1000

    feat: Path operation

--- main.py
+++ main.py
@@ -3,6 +3,33 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
+# Path Operation / Route
+# Decorator converts the function into a path operation
+# Makes it an API that can be called
+# decorator - object.HTTPFunction(Params)
+# Path operation converts function into API
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    # Converts it to JSON
+    return {"message": "Hello World!"}
+
+
+@app.get("/posts")
+def get_posts():
+    return [
+        {
+            "id": 1,
+            "title": "Post 1",
+            "body": "This is post 1",
+        },
+        {
+            "id": 2,
+            "title": "Post 2",
+            "body": "This is post 2",
+        },
+        {
+            "id": 3,
+            "title": "Post 3",
+            "body": "This is post 3",
+        },
+    ]
