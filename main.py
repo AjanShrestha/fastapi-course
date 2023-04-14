58017f05d5b300b7d42b87388530f4819052f4c7
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-14 15:58:17 +1000

    feat: delete a post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from fastapi import FastAPI, status, HTTPException
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
 from random import randrange
@@ -25,6 +25,13 @@ def find_post(id: int):
     for post in my_posts:
         if post["id"] == id:
             return post
+    return None
+
+
+def find_index_post(id: int):
+    for index, post in enumerate(my_posts):
+        if post["id"] == id:
+            return index
     return None
 
 
@@ -69,3 +76,18 @@ def get_post(id: int):
             )
         )
     return {"post_detail": post}
+
+
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    index = find_index_post(id)
+    if index is None:
+        raise (
+            HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail=f"Post with id: {id} was not found",
+            )
+        )
+    my_posts.pop(index)
+    # Normally we don't return anything, on delete success
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
