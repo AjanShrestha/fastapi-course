e66297d757a0a09b1dbf6fd2cebb4b86d0f4b7ee
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-23 17:54:20 +1000

    feat: connect db to CRUD operation

--- app/main.py
+++ app/main.py
@@ -73,18 +73,22 @@ def get_posts():
 # convert it into python dictionarym, and
 # store it inside variable paylopad
 def create_posts(post: Post):
-    # Pydantic model to Python dict
-    post_dict = post.dict()
-    post_dict["id"] = randrange(1, 9999999)
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+    # parameterized and sanitize
+    # security: make it safe from SQL injection
+    new_post = cursor.execute(
+        """INSERT INTO posts (title, content, published) VALUES (%s, %s, %s) RETURNING *""",
+        (post.title, post.content, post.published),
+    ).fetchone()
+    # commit to save it in the database
+    conn.commit()
+    return {"data": new_post}
 
 
 # path parameter
 @app.get("/posts/{id}")
 # convert the id into int as default type is string
 def get_post(id: int):
-    post = find_post(id)
+    post = cursor.execute("""SELECT * FROM posts WHERE id=%s""", (str(id),)).fetchone()
     if not post:
         raise (
             HTTPException(
@@ -97,30 +101,37 @@ def get_post(id: int):
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(id: int):
-    index = find_index_post(id)
-    if index is None:
+    deleted_post = cursor.execute(
+        """DELETE FROM posts WHERE id=%s RETURNING *""", (str(id),)
+    ).fetchone()
+    conn.commit()
+
+    if deleted_post is None:
         raise (
             HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=f"Post with id: {id} was not found",
             )
         )
-    my_posts.pop(index)
+
     # Normally we don't return anything, on delete success
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
 @app.put("/posts/{id}")
 def update_post(id: int, post: Post):
-    index = find_index_post(id)
-    if index is None:
+    updated_post = cursor.execute(
+        """UPDATE posts SET title=%s, content=%s, published=%s WHERE id=%s RETURNING *""",
+        (post.title, post.content, post.published, str(id)),
+    ).fetchone()
+    conn.commit()
+
+    if updated_post is None:
         raise (
             HTTPException(
                 status_code=status.HTTP_404_NOT_FOUND,
                 detail=f"Post with id: {id} was not found",
             )
         )
-    post_dict = post.dict()
-    post_dict["id"] = id
-    my_posts[index] = post_dict
-    return {"data": post_dict}
+
+    return {"data": updated_post}
