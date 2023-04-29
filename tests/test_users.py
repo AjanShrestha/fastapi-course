8acaed8606bb4ea343bf014e77cacd874f3695dd
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 06:42:38 +1000

    feat: Test user creating using pydantic model

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
 from fastapi.testclient import TestClient
 
+from app import schemas
 from app.main import app
 
 
@@ -11,3 +12,12 @@ def test_root():
     print(res.json().get("message"))
     assert res.status_code == 200
     assert res.json().get("message") == "Hello World!"
+
+
+def test_create_user():
+    res = client.post(
+        "/users", json={"email": "newuser@xyz.com", "password": "password"}
+    )
+    new_user = schemas.UserOut(**res.json())
+    assert new_user.email == "newuser@xyz.com"
+    assert res.status_code == 201
