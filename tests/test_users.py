6d91fdc92c3d1fc50184358e92f783bf96f57b30
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 07:15:05 +1000

    refactor: Move database related codes into separate file

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,46 +1,6 @@
-import pytest
-from fastapi.testclient import TestClient
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
 from app import schemas
-from app.config import settings
-from app.database import Base, get_db
-from app.main import app
 
-SQLACHEMY_DATABASE_URL = f"{settings.database}://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
-
-# engine - responsible for SQLAlchemy to connect to the database
-engine = create_engine(SQLACHEMY_DATABASE_URL)
-
-# talk to the database via session
-TestingSessionLocal = sessionmaker(autoflush=False, bind=engine)
-
-
-@pytest.fixture
-def session():
-    # run our code before we run our test
-    Base.metadata.drop_all(bind=engine)
-    Base.metadata.create_all(bind=engine)
-    db = TestingSessionLocal()
-    try:
-        yield db
-        # run our code after our test finishes
-    finally:
-        db.close()
-
-
-@pytest.fixture
-def client(session):
-    # Dependency
-    def override_get_db():
-        db = TestingSessionLocal()
-        try:
-            yield session
-        finally:
-            session.close()
-    app.dependency_overrides[get_db] = override_get_db
-    yield TestClient(app)
+from .database import client, session
 
 
 def test_root(client):
