edb4c42196427596ca4418be691de6995079cfa1
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 06:55:02 +1000

    feat: setup test database

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,8 +1,35 @@
 from fastapi.testclient import TestClient
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-from app import schemas
+from app import models, schemas
+from app.config import settings
+from app.database import Base, get_db
 from app.main import app
 
+SQLACHEMY_DATABASE_URL = f"{settings.database}://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
+
+# engine - responsible for SQLAlchemy to connect to the database
+engine = create_engine(SQLACHEMY_DATABASE_URL)
+
+# talk to the database via session
+TestingSessionLocal = sessionmaker(autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+# Dependency
+def override_get_db():
+    db = TestingSessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
