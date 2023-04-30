f63de8f257a0e96ab969d87b2b8d47c5657a049d
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 10:08:38 +1000

    refactor: Move db related fixture to conftest file
    - Special file that provides the fixture via package scope

--- tests/test_users.py
+++ tests/test_users.py
@@ -3,19 +3,6 @@ from jose import jwt
 
 from app import schemas
 from app.config import settings
-from .database import client, session
-
-
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "newuser@xyz.com", "password": "password"}
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user["password"] = user_data["password"]
-
-    return new_user
 
 
 def test_root(client):
