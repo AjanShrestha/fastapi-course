9b1923995859104d5f684d011ea0e59cbe75ef27
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 11:34:07 +1000

    feat: test unauthorized posts

--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,5 +1,21 @@
+from app import schemas
+
+
 def test_get_all_posts(authorized_client, test_posts):
     res = authorized_client.get("/posts/")
 
     assert res.status_code == 200
     assert len(res.json()) == len(test_posts)
+
+    def validate(post):
+        return schemas.PostOut(**post)
+
+    # asserting the correct response types
+    posts_map = map(validate, res.json())
+    posts_lists = list(posts_map)
+
+
+def test_unauthorized_get_all_posts(client, test_posts):
+    res = client.get("/posts/")
+
+    assert res.status_code == 401
