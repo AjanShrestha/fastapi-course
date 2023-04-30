4727438dfc1bc58c0098bf2e70577583ac752471
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 11:18:59 +1000

    feat: Test posts with db entry

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,6 +3,7 @@ from fastapi.testclient import TestClient
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
+from app import models
 from app.config import settings
 from app.database import Base, get_db
 from app.main import app
@@ -65,3 +66,35 @@ def token(test_user):
 def authorized_client(client, token):
     client.headers = {**client.headers, "Authorization": f"Bearer {token}"}
     return client
+
+
+@pytest.fixture()
+def test_posts(session, test_user):
+    posts_data = [
+        {"title": "1st title", "content": "1st content", "owner_id": test_user["id"]},
+        {"title": "2nd title", "content": "2nd content", "owner_id": test_user["id"]},
+        {"title": "3rd title", "content": "3rd content", "owner_id": test_user["id"]},
+    ]
+
+    def create_post_model(post):
+        return models.Post(**post)
+
+    post_map = map(create_post_model, posts_data)
+    posts = list(post_map)
+    session.add_all(posts)
+    # session.add_all(
+    #     [
+    #         models.Post(
+    #             title="1st title", content="1st content", owner_id=test_user["id"]
+    #         ),
+    #         models.Post(
+    #             title="2nd title", content="2nd content", owner_id=test_user["id"]
+    #         ),
+    #         models.Post(
+    #             title="3rd title", content="3rd content", owner_id=test_user["id"]
+    #         ),
+    #     ]
+    # )
+    session.commit()
+
+    return posts
