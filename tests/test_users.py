d74a7642598812906469711511dc87cc829f382d
Ajan Shrestha <1964074+AjanShrestha@users.noreply.github.com>
2023-04-30 10:29:50 +1000

    feat: test incorrect login

--- tests/test_users.py
+++ tests/test_users.py
@@ -36,3 +36,22 @@ def test_login_user(client, test_user):
     )
     id = payload.get("user_id")
     assert id == test_user["id"]
+
+
+@pytest.mark.parametrize(
+    "email, password, status_code",
+    [
+        ("incorrect_email@email.com", "password", 403),
+        ("newuser@xyz.com", "incorrect_password", 403),
+        (None, "wrongpassword", 422),
+        ("newuser@xyz.com", None, 422),
+    ],
+)
+def test_incorrect_login(client, test_user, email, password, status_code):
+    res = client.post(
+        "/login/",
+        data={"username": email, "password": password},
+    )
+
+    assert res.status_code == status_code
+    # assert res.json().get("detail") == "Invalid credentials"
